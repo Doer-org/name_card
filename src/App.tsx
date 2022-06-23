b9558eecb488665f5dc9bcccfbfd8388b4beeda7
nisi0929 <kengo279@icloud.com>
2022-06-24 01:53:45 +0900

    finish footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Header } from './components/Layouts/Header'
+import { Footer } from './components/Layouts/Footer'
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="text-center font-bold">name_card APP</div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Layouts/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="text-center bg-yellow-300 py-2 md:py-10 md:text-xl">
+      doer-2022
+    </div>
+  )
+}
