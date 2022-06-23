0e195b7d1443f577b7fd37bd87ff543fe6caeece
nisi0929 <kengo279@icloud.com>
2022-06-24 01:47:45 +0900

    finish header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.css'
+import { Header } from './components/Layouts/Header'
 function App() {
   return (
     <div className="App">
-     <div className='text-center font-bold'>name_card APP</div>
+      <Header />
+      <div className="text-center font-bold">name_card APP</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,3 +1,14 @@
+import logo from '../../assets/img/logo.svg'
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="bg-yellow-300">
+      <div className="py-1 md:py-5">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-20 h-20 md:w-28 md:h-28 mx-auto"
+        />
+      </div>
+    </div>
+  )
 }
